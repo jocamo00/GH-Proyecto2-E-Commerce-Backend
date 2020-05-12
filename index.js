d96d9d01e428ef2146f4c46b3b145f9295fb4137
Jose Carreres <jose.carreres.81@gmail.com>
2020-05-12 02:14:14 +0200

    Estructura inicial del proyecto y conexión a la Base de Datos

---
+++ index.js
@@ -0,0 +1,35 @@
+//#region require
+const mongoose = require('mongoose')
+const express  = require('express');
+const cors     = require('cors')
+const app      = express();
+//#endregion
+
+
+app.use(cors())
+//Recoje la petición y la convierte en JSON
+app.use(express.json());
+app.use('/public', express.static('public')) // para darle acceso desde el navegar a public y poder ver las imagenes
+
+
+// Rutas padre
+
+
+//#region Configuración del puerto
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`Escuchando Puerto ${port}`))
+//#endregion
+
+// valor de la variable de entorno de la key del JWT
+// console.log(process.env.SECRET_KEY_JWT_NETFLIX_API)
+
+
+//#region Conexión a la BD
+mongoose.connect('mongodb://localhost:27017/ecommercedb', { useNewUrlParser: true, 
+                                                            useUnifiedTopology: true, 
+                                                            useFindAndModify: false, 
+                                                            useCreateIndex: true})
+
+    .then(() => console.log('Conectado correctamente a MongoDB'))
+    .catch(() => console.log('Error al conectarse a MongoDB'))
+//#endregion
